00febd8ca4716604c1d12937f68ac719cac0b665
Liam <liargent@hotmail.com>
2023-02-12 13:07:41 +0000

    Create functions for fading in, out and removing the overlay

--- src/components/centeredOverlay/script.js
+++ src/components/centeredOverlay/script.js
@@ -7,12 +7,23 @@ export default function createOverlay(parent) {
 
   // When overlay is clicked (as opposed to anything on top of it), turns invisible and is removed
   overlay.addEventListener('click', (e) => {
-    if (e.target === overlay) {
-      overlay.classList.add('hidden');
-      overlay.addEventListener('transitionend', () => overlay.remove());
-    }
+    if (e.target === overlay) fadeOutAndRemove(overlay);
   });
 
-  overlay.classList.remove('hidden');
   return overlay;
+}
+
+export function fadeIn(overlay) {
+  // Wrapped in a timeout as per MDN "Using CSS Transitions" guide.
+  // "Care should be taken when using a transition immediately after... adding an element to the DOM using .appendChild()."
+  // "This is treated as if the element was in its final state... use setTimeout()"!
+  // Spent nearly 2 hours trying to figure out what was causing this.
+  setTimeout(() => {
+    overlay.classList.remove('hidden');
+  }, 5);
+}
+
+export function fadeOutAndRemove(overlay) {
+  overlay.classList.add('hidden');
+  overlay.addEventListener('transitionend', () => overlay.remove());
 }
