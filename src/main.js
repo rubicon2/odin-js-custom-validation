d62b8f9219951d00338119233ab76ed2fe1d862b
Liam <liargent@hotmail.com>
2023-02-23 21:47:28 +0000

    Clean up validation code and show errors when attempting to submit form with empty fields

--- src/main.js
+++ src/main.js
@@ -13,6 +13,11 @@ import { listenForValidityOnFocusOut } from './modules/DOMEventHelpers';
 
 const body = document.querySelector('body');
 let detailsForm = null;
+let emailFormItem = null;
+let countryFormItem = null;
+let postcodeFormItem = null;
+let passwordFormItem = null;
+let confirmPasswordFormItem = null;
 
 let showMenuButton = createButton('Show Form', 'button');
 body.appendChild(showMenuButton);
@@ -35,107 +40,126 @@ function clearFormItemError(formItem) {
   formItem.error.classList.add('hidden');
 }
 
+function validateEmail() {
+  if (emailFormItem.input.validity.valueMissing) {
+    setFormItemError(emailFormItem, 'Please enter an email address');
+  } else if (emailFormItem.input.validity.typeMismatch) {
+    setFormItemError(
+      emailFormItem,
+      'Please enter a proper email address with an @ sign and everything...'
+    );
+  } else {
+    clearFormItemError(emailFormItem);
+    return true;
+  }
+  return false;
+}
+
+function validateCountry() {
+  if (countryFormItem.input.validity.valueMissing) {
+    setFormItemError(
+      countryFormItem,
+      'Please enter a real or fictional country.'
+    );
+  } else {
+    clearFormItemError(countryFormItem);
+    return true;
+  }
+  return false;
+}
+
+function validatePostcode() {
+  if (postcodeFormItem.input.validity.valueMissing) {
+    setFormItemError(postcodeFormItem, 'Please enter a postcode.');
+  } else if (postcodeFormItem.input.validity.patternMismatch) {
+    setFormItemError(postcodeFormItem, 'Please enter a VALID UK postcode.');
+  } else {
+    clearFormItemError(postcodeFormItem);
+    return true;
+  }
+  return false;
+}
+
+function validatePassword() {
+  // If new password is entered/changed, always clear any confirmPasswordFormItem error
+  clearFormItemError(confirmPasswordFormItem);
+  if (passwordFormItem.input.validity.valueMissing) {
+    setFormItemError(passwordFormItem, 'Please enter a password.');
+  } else if (passwordFormItem.input.validity.patternMismatch) {
+    setFormItemError(passwordFormItem, 'Pattern mismatch bro');
+  } else {
+    clearFormItemError(passwordFormItem);
+    return true;
+  }
+  return false;
+}
+
+function validateConfirmPassword() {
+  if (passwordFormItem.input.value != confirmPasswordFormItem.input.value) {
+    setFormItemError(confirmPasswordFormItem, 'Passwords do not match.');
+    return false;
+  } else {
+    clearFormItemError(confirmPasswordFormItem);
+    return true;
+  }
+}
+
 function createDetailsForm(parent) {
   let overlay = createOverlay(parent);
   detailsForm = createForm(overlay);
   detailsForm.noValidate = true;
 
-  let emailFormItem = createFormItem(
+  emailFormItem = createFormItem(
     detailsForm,
     'Email',
     createInput('email', 'email', true)
   );
-  listenForValidityOnFocusOut(emailFormItem.input, () => {
-    if (emailFormItem.input.validity.valueMissing) {
-      setFormItemError(emailFormItem, 'Please enter an email address');
-    } else if (emailFormItem.input.validity.typeMismatch) {
-      setFormItemError(
-        emailFormItem,
-        'Please enter a proper email address with an @ sign and everything...'
-      );
-    } else {
-      clearFormItemError(emailFormItem);
-      return true;
-    }
-    return false;
-  });
+  listenForValidityOnFocusOut(emailFormItem.input, validateEmail);
 
-  let countryFormItem = createFormItem(
+  countryFormItem = createFormItem(
     detailsForm,
     'Country',
     createInput('text', 'country', true)
   );
-  listenForValidityOnFocusOut(countryFormItem.input, () => {
-    if (countryFormItem.input.validity.valueMissing) {
-      setFormItemError(
-        countryFormItem,
-        'Please enter a real or fictional country.'
-      );
-    } else {
-      clearFormItemError(countryFormItem);
-      return true;
-    }
-    return false;
-  });
+  listenForValidityOnFocusOut(countryFormItem.input, validateCountry);
 
-  let postcodeFormItem = createFormItem(
+  postcodeFormItem = createFormItem(
     detailsForm,
     'Postcode',
     createInput('text', 'postcode', true)
   );
   postcodeFormItem.input.pattern =
     '[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]? [0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}';
-  listenForValidityOnFocusOut(postcodeFormItem.input, () => {
-    if (postcodeFormItem.input.validity.valueMissing) {
-      setFormItemError(postcodeFormItem, 'Please enter a postcode.');
-    } else if (postcodeFormItem.input.validity.patternMismatch) {
-      setFormItemError(postcodeFormItem, 'Please enter a VALID UK postcode.');
-    } else {
-      clearFormItemError(postcodeFormItem);
-      return true;
-    }
-    return false;
-  });
+  listenForValidityOnFocusOut(postcodeFormItem.input, validatePostcode);
 
-  let passwordFormItem = createFormItem(
+  passwordFormItem = createFormItem(
     detailsForm,
     'Password',
     createInput('password', 'password', true)
   );
-  // This pattern seems to be producing some weird results
-  passwordFormItem.input.pattern =
-    '(?=^.{8,}$)((?=.*d)|(?=.*W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$';
-  listenForValidityOnFocusOut(passwordFormItem.input, () => {
-    if (passwordFormItem.input.validity.valueMissing) {
-      setFormItemError(passwordFormItem, 'Please enter a password.');
-    } else if (passwordFormItem.input.validity.patternMismatch) {
-      setFormItemError(passwordFormItem, 'Pattern mismatch bro');
-    } else {
-      clearFormItemError(passwordFormItem);
-      return true;
-    }
-    return false;
-  });
 
-  let confirmPasswordFormItem = createFormItem(
+  // Create before setting up passwordFormItem so can check if passwords match when either input event is triggered
+  confirmPasswordFormItem = createFormItem(
     detailsForm,
     'Confirm password',
     createInput('password', 'confirm', true)
   );
-  listenForValidityOnFocusOut(confirmPasswordFormItem.input, () => {
-    if (passwordFormItem.input.value != confirmPasswordFormItem.input.value) {
-      setFormItemError(confirmPasswordFormItem, 'Passwords do not match.');
-    } else {
-      clearFormItemError(confirmPasswordFormItem);
-      return true;
-    }
-    return false;
-  });
+
+  passwordFormItem.input.pattern =
+    '(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$';
+  listenForValidityOnFocusOut(passwordFormItem.input, validatePassword);
+
+  listenForValidityOnFocusOut(
+    confirmPasswordFormItem.input,
+    validateConfirmPassword
+  );
 
   createCancelSubmitButtons(
     detailsForm,
     () => fadeOutAndRemove(overlay),
-    () => submitDetailsForm()
+    () => {
+      if (checkDetailsFormValidity()) detailsForm.submit();
+    }
   );
 
   // Fade in once form has been constructed
@@ -143,4 +167,17 @@ function createDetailsForm(parent) {
   return overlay;
 }
 
-function submitDetailsForm() {}
+function checkDetailsFormValidity() {
+  let isEmailValid = validateEmail();
+  let isCountryValid = validateCountry();
+  let isPostcodeValid = validatePostcode();
+  let isPasswordValid = validatePassword();
+  let isConfirmPasswordValid = validateConfirmPassword();
+  return (
+    isEmailValid &&
+    isCountryValid &&
+    isPostcodeValid &&
+    isPasswordValid &&
+    isConfirmPasswordValid
+  );
+}
